a8278678deccb4c174d4248cb9cb5cd3dc661ea1
abcde2000 <2966308756@qq.com>
2019-10-29 20:43:12 +0800

    https://github.com/abcde2000/000.git

--- haoaini.c
+++ haoaini.c
@@ -1,1 +1,11 @@
-#include
+#include<stdio.h>
+int main(void)
+{
+    int a=20,b=30,c=40,d=50'sum;
+    float aver;
+
+    sum=a+b+c+d;
+    aver=sum/4.0f;
+    printf("sum is%d,average is %.1f\n",sum,aver);
+    return 0;
+}
---
+++ wrzz.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main(void)
+{
+	float w,x;
+	printf("Enter x:\n");
+	scanf("%f",&x);
+	if(x<=50);
+		printf("w is %f\n",0.53*x);
+		if(x>50);
+			printf("w is %f\n",1.11*x-29);
+			return 0;
+}
