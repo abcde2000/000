eb15a426b00bc5f354daa9e4788fac2f8f8c1b13
abcde2000 <2966308756@qq.com>
2019-11-04 20:35:58 +0800

    https://github.com/abcde2000/000.git

---
+++ wrsb.c
@@ -0,0 +1,26 @@
+#include<stdio> 
+int main(void)
+{
+    int w,t,x;
+    printf("Enter t,x:\n");
+    scanf("%d%d",%t,%x);
+    if(x<=3&&t>5){
+        w=10+2*t
+    };
+    or(x<=3&&t<=5){
+        w=10
+    };
+    if(3<x<=13&&t>5){
+        w=2*x+2*t+4
+    };
+    or(3<x<=13&&t<=5){
+        w=2*x+4
+    };
+    if(x>13&&t>5){
+        w=3*x+2*t-9
+    };
+    or(x>13&&t<=5){
+        w=3*x-9
+    };
+    return 0
+}
